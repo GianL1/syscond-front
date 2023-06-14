95de6288570a5037c3e580c878723b471558ef39
Gyan Lima <gyan.patrick1@gmail.com>
2023-06-14 20:58:14 -0300

    tela de login

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -20,5 +20,8 @@ export default {
     align-items: center;
     justify-content: center;
     width: 100%;
+    position: absolute;
+    width: 100%;
+    bottom: 0
   }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,35 +1,45 @@
 <template>
-  <h2>Login</h2>
+  <div class="login-content">
+    <div id="login-form">
+      <h2>Login</h2>
 
-  <form @submit.prevent="login" >
-    <label for="email">Email: </label>
-    <input type="email" name="email">
+      <form @submit.prevent="login">
 
-    <label for="password">Password: </label>
-    <input type="text" name="password">
+        <label for="email">Email: </label>
+        <input type="email" name="email" v-model="email" required>
 
-    <InputSubmit :titulo_botao="Login"></InputSubmit>
+        <label for="password">Password: </label>
+        <input type="password" v-model="password" name="password" required>
 
-  </form>
+        <InputSubmit :titulo_botao="Login"></InputSubmit>
+
+      </form>
+    </div>
+
+  </div>
+
 
 </template>
 <script>
   import http from '@/services/http';
-  import {reactive} from "vue";
   import InputSubmit from "@/components/form/InputSubmit";
   import {useAuth} from "@/stores/auth";
 
   const auth = useAuth();
-  const user = reactive({
-    email:'gyan.patrick1@gmail.com',
-    password:'thiego01'
-  })
 
   export default {
     components: {InputSubmit},
+    data(){
+      return{
+        email:'',
+        password:''
+      }
+    },
     methods:{
       async login() {
         try {
+
+          const user = JSON.stringify({email: this.email, password: this.password})
           const {data} = await http.post('/auth/login', user)
           auth.setToken(data.token);
           auth.setUser(data.user);
@@ -49,3 +59,62 @@
   }
 
 </script>
+<style scoped>
+
+ *{
+   margin: 0;
+   padding: 0;
+   border: 0;
+   box-sizing: border-box;
+   font-family: Helvetica;
+   color: #323232;
+ }
+
+  .login-content{
+    background-image: url('../../public/img/background-image.jpg');
+    background-size: cover;
+    background-position-y: -200px;
+  }
+
+  input:focus{
+    outline: none;
+  }
+
+  #login-form{
+    background-color: white;
+    width: 400px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 20px 30px;
+    margin-top: 10vh;
+    border-radius: 10px;
+    text-align: center;
+  }
+
+  form{
+    margin-top: 30px;
+    margin-bottom: 40px;
+  }
+
+  label,input{
+    display: block;
+    width: 100%;
+    text-align: left;
+
+  }
+  label{
+    font-weight: bold;
+    font-size: .8rem;
+  }
+
+  input{
+    border-bottom: 2px solid #323232;
+    padding: 10px;
+    font-size: 1rem;
+    margin-bottom: 20px;
+  }
+
+  input:focus{
+    border-bottom: 2px solid blue;
+  }
+</style>
